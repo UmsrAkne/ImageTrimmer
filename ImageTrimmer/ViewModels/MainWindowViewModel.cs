09332ec79fa3517af1dfbf9c54153d74632dd3c2
Akn <sibata486@gmail.com>
2024-03-23 12:51:04 +0900

    init /

---
+++ ImageTrimmer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+
+namespace ImageTrimmer.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Image Trimmer";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
